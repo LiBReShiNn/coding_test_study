7191c7291a5ca2555de482718f16a35c81c74474
LiBReShiNn <shinhj1991@gmail.com>
2022-07-04 14:17:38 +0900

    study. dart. decorator pattern

---
+++ src/DesignPattern/Decorator.java
@@ -0,0 +1,71 @@
+//package DesignPattern;
+//
+//public class Decorator {
+//
+//    //interface
+//    class Cat{
+//        void draw(){}
+//        void talk(){}
+//    }
+//
+//    class SimpleCat extends Cat{
+//        @override
+//        void draw(){
+//            // Draw cat shape
+//        }
+//        @override
+//        void talk(){
+//            print('Im cat');
+//        }
+//    }
+//
+//    abstract class CatDecorator implements Cat{
+//        final Cat _cat;
+//        CatDecorator(this._cat);
+//    }
+//
+//    class CheeseDecoratorCat extends CatDecorator {
+//        CheeseDecoratorCat(Cat cat) : super(cat);
+//
+//        @override
+//        void draw(){
+//            _cat.draw();
+//            _drawCheesePattern();
+//        }
+//        void _drawCheesePattern(){
+//            // Draw Cheese pattern
+//        }
+//
+//        @override
+//        void talk(){
+//            _cat.talk();
+//            print('Im cheese cat');
+//        }
+//    }
+//
+//    class OddEyesDecoratorCat extends CatDecorator {
+//        OddEyesDecoratorCat(Cat cat) : super(cat);
+//
+//        @override
+//        void draw(){
+//            _cat.draw();
+//            _drawOddEyes();
+//        }
+//        void _drawOddEyes(){
+//            // Draw Odd eyes
+//        }
+//
+//        @override
+//        void talk(){
+//            _cat.talk();
+//            print('Im Odd eyes cat');
+//        }
+//    }
+//
+//
+//    void main() {
+//        Cat decoratedCat = OddEyesDecoratorCat(CheeseDecoratorCat(SimpleCat()));
+//        decoratedCat.talk();
+//    }
+//
+//}
