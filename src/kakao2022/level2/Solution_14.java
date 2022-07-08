152bc9f9f8320028eaede4759e3bb6c82dc991ed
LiBReShiNn <shinhj1991@gmail.com>
2022-07-08 20:35:28 +0900

    kakao level test 2

---
+++ src/kakao2022/level2/Solution_14.java
@@ -0,0 +1,63 @@
+package kakao2022.level2;
+
+import java.util.*;
+
+public class Solution_14 {
+
+
+    public int[] solution(int[] fees, String[] records) {
+        int[] answer = {};
+
+        int normalTime = fees[0];
+        int normalFee = fees[1];
+        int dt = fees[2];
+        int feeByDt = fees[3];
+
+        HashMap<String, Stack<Integer>> timeCount = new HashMap<>();
+        HashMap<String, Integer> totalTimes = new HashMap<>();
+        for (String record : records) {
+            String[] split = record.split(" ");
+            int time = getTime(split[0]);
+            String number = split[1];
+            if (timeCount.containsKey(number)) {
+                Stack<Integer> stack = timeCount.get(number);
+                if (stack.isEmpty()) {
+                    stack.add(time);
+                } else {
+                    int t = time - stack.pop();
+                    totalTimes.put(number, totalTimes.get(number) + t);
+                }
+            } else {
+                totalTimes.put(number, 0);
+                Stack<Integer> stack = new Stack<>();
+                stack.add(time);
+                timeCount.put(number, stack);
+            }
+        }
+        for (String key : timeCount.keySet()) {
+            if (!timeCount.get(key).isEmpty()) {
+                int t = 23 * 60 + 59 - timeCount.get(key).pop();
+                totalTimes.put(key, totalTimes.get(key) + t);
+            }
+        }
+        ArrayList<String> keys = new ArrayList<>(totalTimes.keySet());
+        Collections.sort(keys);
+        answer = new int[keys.size()];
+        int idx = 0;
+        for (String key : keys) {
+            int totalTime = totalTimes.get(key);
+            if (normalTime >= totalTime) {
+                answer[idx++] = normalFee;
+            } else {
+                answer[idx++] = normalFee + ((int) Math.ceil((double) (totalTime - normalTime) / dt)) * feeByDt;
+            }
+        }
+        return answer;
+    }
+
+    private int getTime(String s) {
+        String[] split = s.split(":");
+        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+    }
+
+}
---
+++ src/kakao2022/level2/Solution_4.java
@@ -0,0 +1,64 @@
+package kakao2022.level2;
+
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+public class Solution_4 {
+    static int[] fees;
+
+    public int[] solution(int[] fees, String[] records) {
+        this.fees = fees;
+
+        LinkedList<String> time = new LinkedList<>();
+        LinkedList<String> number = new LinkedList<>();
+        TreeMap<String, Integer> pay = new TreeMap<>();
+        for (int i = 0; i < records.length; i++) {
+            String[] info = records[i].split(" ");
+            if (!pay.containsKey(info[1])) {
+                pay.put(info[1], 0);
+            }
+
+            if (!number.contains(info[1])) {
+                time.add(info[0]);
+                number.add(info[1]);
+                continue;
+            }
+            int index = number.indexOf(info[1]);
+            String start_time = time.get(index);
+            time.remove(index);
+            number.remove(index);
+            pay.put(info[1], pay.get(info[1]) + calc_diff(start_time, info[0]));
+        }
+
+        if (!number.isEmpty()) {
+            for (int i = 0; i < number.size(); i++) {
+                System.out.println(time.get(i));
+                pay.put(number.get(i), pay.get(number.get(i)) + calc_diff(time.get(i), "23:59"));
+            }
+        }
+        int[] answer = new int[pay.size()];
+        int i = 0;
+        for (String key : pay.keySet()) {
+            answer[i++] = calc_money(pay.get(key));
+        }
+        return answer;
+    }
+
+    static int calc_money(int diff) {
+
+        if (diff <= fees[0]) return fees[1];
+        diff -= fees[0];
+        double n = diff * 1.0 / fees[2];
+        return fees[1] + ((int) Math.ceil(n)) * fees[3];
+    }
+
+    static int calc_diff(String start_time, String end_time) {
+        String[] start = start_time.split(":");
+        String[] end = end_time.split(":");
+
+        int st = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
+        int et = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
+
+        return et - st;
+    }
+}
