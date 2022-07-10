6613ac10fc4a091b09fd959747ed18c2cd9d5686
LiBReShiNn <shinhj1991@gmail.com>
2022-07-11 04:14:55 +0900

    stream.of(stream, stream).mapTo... 분기 하여 작업 할 수 있는 스트림을 of로 묶어서 다시 스트림으로 만든다.

---
+++ src/devmatching2021/level_1/Solution_5.java
@@ -0,0 +1,24 @@
+package devmatching2021.level_1;
+
+public class Solution_5 {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int zero = 0;
+        int matched = 0;
+        for (int l : lottos) {
+            if (l == 0) {
+                zero++;
+            } else {
+                for (int w : win_nums) {
+                    if (l == w) {
+                        matched++;
+                        break;
+                    }
+                }
+            }
+        }
+        int min = matched;
+        int max = matched + zero;
+        int[] answer = {Math.min(7 - max, 6), Math.min(7 - min, 6)};
+        return answer;
+    }
+}
