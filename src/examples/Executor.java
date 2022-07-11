101d9c078cb8acb9b6fbb909e734a2dea0206c17
LiBReShiNn <shinhj1991@gmail.com>
2022-07-11 22:49:28 +0900

    행렬의 덧셈은 같은 좌표에 위치하는 원소간 더한다. 곱은 앞의 행과 뒤의 열이 같아야 한다. 행열의 덧셈시 결과는 첫 행열의 [열][행]. char[] = string.toCharArray(). 마스킹 등 작업시 String->char[]

--- src/examples/Executor.java
+++ src/examples/Executor.java
@@ -1,21 +1,24 @@
 package examples;
 
-import kakao2020.level_1_1.Solution;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Executor {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        Solution_stars stars = new Solution_stars();
-        Solution_distance distance = new Solution_distance();
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        Solution_stars stars = new Solution_stars();
+//        Solution_distance distance = new Solution_distance();
+        Solution_matrix matrixSum = new Solution_matrix();
+        Solution_masking masking = new Solution_masking();
 //        stars.stars(a, b);
-        System.out.println(Arrays.toString(distance.solution(a, b)));
+//        int[][] arr1 = {{1, 2}, {2, 3}};
+//        int[][] arr2 = {{3, 4}, {5, 6}};
+        int[][] arr1 = {{1}, {2}};
+        int[][] arr2 = {{3}, {4}};
+//        System.out.println(Arrays.deepToString(matrixSum.solution(arr1, arr2)));
+        System.out.println(masking.solution("01033334444"));
+        System.out.println(masking.solution("027778888"));
 
     }
 
