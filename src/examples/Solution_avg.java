5d476c947c43107c4304bee246150b399f0ba44f
HyeJiShin <h.j.shin@plendar.com>
2022-07-12 01:11:49 +0900

    연습문제 쉬운거...

---
+++ src/examples/Solution_avg.java
@@ -0,0 +1,12 @@
+package examples;
+
+import java.util.Arrays;
+
+public class Solution_avg {
+
+    public double solution(int[] arr) {
+
+        return Arrays.stream(arr).asDoubleStream().average().getAsDouble();
+
+    }
+}
