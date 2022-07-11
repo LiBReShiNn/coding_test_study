4fadb98995e40fcf99fe42d9b56c887e327356cc
HyeJiShin <h.j.shin@plendar.com>
2022-07-11 22:49:28 +0900

    행렬의 덧셈은 같은 좌표에 위치하는 원소간 더한다. 곱은 앞의 행과 뒤의 열이 같아야 한다. 행열의 덧셈시 결과는 첫 행열의 [열][행]. char[] = string.toCharArray(). 마스킹 등 작업시 String->char[]

---
+++ src/examples/Solution_masking.java
@@ -0,0 +1,15 @@
+package examples;
+
+public class Solution_masking {
+
+    public String solution(String phone_number) {
+
+        int index = phone_number.length() - 4;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < index; i++) {
+            sb.append("*");
+        }
+        sb.append(phone_number.substring(index));
+        return sb.toString();
+    }
+}
