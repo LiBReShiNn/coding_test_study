62dab956bb14a584d7b9a533343f402149f18c31
LiBReShiNn <shinhj1991@gmail.com>
2022-07-28 01:03:46 +0900

    런타임시 제네릭 소거로 인한 시그니처 불일치를 컴파일러가 해결하는 방식

---
+++ src/issue_check/Genaric_Bridge_Method.java
@@ -0,0 +1,60 @@
+package issue_check;
+
+import java.util.Comparator;
+
+public class Genaric_Bridge_Method implements Comparator<Integer> {
+
+    /**
+     * Compares its two arguments for order.  Returns a negative integer,
+     * zero, or a positive integer as the first argument is less than, equal
+     * to, or greater than the second.<p>
+     * <p>
+     * In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.<p>
+     * <p>
+     * The implementor must ensure that <tt>sgn(compare(x, y)) ==
+     * -sgn(compare(y, x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>compare(x, y)</tt> must throw an exception if and only
+     * if <tt>compare(y, x)</tt> throws an exception.)<p>
+     * <p>
+     * The implementor must also ensure that the relation is transitive:
+     * <tt>((compare(x, y)&gt;0) &amp;&amp; (compare(y, z)&gt;0))</tt> implies
+     * <tt>compare(x, z)&gt;0</tt>.<p>
+     * <p>
+     * Finally, the implementor must ensure that <tt>compare(x, y)==0</tt>
+     * implies that <tt>sgn(compare(x, z))==sgn(compare(y, z))</tt> for all
+     * <tt>z</tt>.<p>
+     * <p>
+     * It is generally the case, but <i>not</i> strictly required that
+     * <tt>(compare(x, y)==0) == (x.equals(y))</tt>.  Generally speaking,
+     * any comparator that violates this condition should clearly indicate
+     * this fact.  The recommended language is "Note: this comparator
+     * imposes orderings that are inconsistent with equals."
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer as the
+     * first argument is less than, equal to, or greater than the
+     * second.
+     * @throws NullPointerException if an argument is null and this
+     *                              comparator does not permit null arguments
+     * @throws ClassCastException   if the arguments' types prevent them from
+     *                              being compared by this comparator.
+     */
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+
+
+    //THIS is a "bridge method"
+//    Comparator의 compare 메소드의 매개변수 타입은 Object로 바뀔 것입니다.
+//    이러한 메소드 시그니처 사이에 불일치를 없애기 위해서
+//    컴파일러는 런타임에 해당 제네릭 타입의 타입 소거를 위한 bridge method를 만들어 줍니다.
+    public int compare(Object a, Object b) {
+        return compare((Integer)a, (Integer)b);
+    }
+}
