2665db094e62c4cb21383069fe3782235b4691d3
LiBReShiNn <shinhj1991@gmail.com>
2022-08-04 01:46:20 +0900

    필드 배열로 관리

--- src/this_is_java/polymorphism/Car.java
+++ src/this_is_java/polymorphism/Car.java
@@ -3,17 +3,47 @@ package this_is_java.polymorphism;
 public class Car {
 
     // 런타임 시 대입된 필드 값의 변화를 통해 다형성을 구현한다.
-    Tire frontRightTire = new Tire(5, "frontRightTire");
-    Tire frontLeftTire = new Tire(4, "frontLeftTire");
-    Tire backRightTire = new Tire(6, "backRightTire");
-    Tire backLeftTire = new Tire(3, "backLeftTire");
+//    Tire frontRightTire = new Tire(5, "frontRightTire");
+//    Tire frontLeftTire = new Tire(4, "frontLeftTire");
+//    Tire backRightTire = new Tire(6, "backRightTire");
+//    Tire backLeftTire = new Tire(3, "backLeftTire");
+
+    Tire[] tires = {
+            new Tire(5, "frontRightTire"),
+            new Tire(4, "frontLeftTire"),
+            new Tire(6, "backRightTire"),
+            new Tire(3, "backLeftTire")
+    };
+
+//    int run() {
+//        System.out.println("car runs");
+//        if (!frontRightTire.roll()) {
+//            stop();
+//            return 1;
+//        }
+//        if (!frontLeftTire.roll()) {
+//            stop();
+//            return 2;
+//        }
+//        if (!backRightTire.roll()) {
+//            stop();
+//            return 3;
+//        }
+//        if (!backLeftTire.roll()) {
+//            stop();
+//            return 4;
+//        }
+//        return 0;
+//    }
 
     int run() {
         System.out.println("car runs");
-        if (!frontRightTire.roll()) {stop(); return 1;}
-        if (!frontLeftTire.roll()) {stop(); return 2;}
-        if (!backRightTire.roll()) {stop(); return 3;}
-        if (!backLeftTire.roll()) {stop(); return 4;}
+        for (int i = 0; i < tires.length; i++) {
+            if (!tires[i].roll()) {
+                stop();
+                return i + 1;
+            }
+        }
         return 0;
     }
 
--- src/this_is_java/polymorphism/Executor.java
+++ src/this_is_java/polymorphism/Executor.java
@@ -5,36 +5,50 @@ public class Executor {
         Car car = new Car();
 
         for (int i = 0; i < 15; i++) {
-
             int problemLocation = car.run();
-
-            switch (problemLocation) {
-                case 0 -> {
-                    continue;
-                }
-                case 1 -> {
-                    System.out.println("frontRightTire change to HankookTire");
-                    car.frontRightTire = new HankookTire(15, "frontRightTire");
-                }
-                case 2 -> {
-                    System.out.println("frontLeftTire change to KumhoTire");
-                    car.frontLeftTire = new KumhoTire(12, "frontLeftTire");
-                }
-                case 3 -> {
-                    System.out.println("backRightTire change to HankookTire");
-                    car.backRightTire = new HankookTire(16, "backRightTire");
-                }
-                case 4 -> {
-                    System.out.println("backLeftTire change to KumhoTire");
-                    car.backLeftTire = new KumhoTire(13, "backLeftTire");
-                }
-                default -> {
-                    System.out.println("ERROR");
-                    return;
+            if (problemLocation != 0) {
+                if (problemLocation % 2 == 0) {
+                    System.out.println(car.tires[problemLocation - 1].location + "Hankook for new one");
+                    car.tires[problemLocation - 1] = new HankookTire(15, car.tires[problemLocation - 1].location);
+                } else {
+                    System.out.println(car.tires[problemLocation - 1].location + "Kumho for new one");
+                    car.tires[problemLocation - 1] = new KumhoTire(12, car.tires[problemLocation - 1].location);
                 }
             }
-            System.out.println("------------------------------------------------");
-            problemLocation = 0;
+            System.out.println("----------------------------");
         }
+
+//        for (int i = 0; i < 15; i++) {
+//
+//            int problemLocation = car.run();
+//
+//            switch (problemLocation) {
+//                case 0 -> {
+//                    continue;
+//                }
+//                case 1 -> {
+//                    System.out.println("frontRightTire change to HankookTire");
+//                    car.frontRightTire = new HankookTire(15, "frontRightTire");
+//                }
+//                case 2 -> {
+//                    System.out.println("frontLeftTire change to KumhoTire");
+//                    car.frontLeftTire = new KumhoTire(12, "frontLeftTire");
+//                }
+//                case 3 -> {
+//                    System.out.println("backRightTire change to HankookTire");
+//                    car.backRightTire = new HankookTire(16, "backRightTire");
+//                }
+//                case 4 -> {
+//                    System.out.println("backLeftTire change to KumhoTire");
+//                    car.backLeftTire = new KumhoTire(13, "backLeftTire");
+//                }
+//                default -> {
+//                    System.out.println("ERROR");
+//                    return;
+//                }
+//            }
+//            System.out.println("------------------------------------------------");
+//            problemLocation = 0;
+//        }
     }
 }
